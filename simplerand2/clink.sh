13c0ccaafb193719f058d95de159f53bf5fea649
Core Programmers <cp@arrakis.ctsu.ox.ac.uk>
2016-07-22 18:47:44 +0100

    tried to bake in libg2c.so.0, but Apache having none of it...

--- simplerand2/clink.sh
+++ simplerand2/clink.sh
@@ -1,4 +1,4 @@
-    #!/bin/bash
+#!/bin/bash
 
 #WEB_ROOT="/user/halsey/public_html/jameslind/trial_chisq"
 WEB_ROOT="/user/cp/public_html/cjb/jameslind"
@@ -16,7 +16,8 @@ gcc -c -g "$FILE_BASE"_c.c
 g77 -c "$FILE_BASE"_f.f
 g77 -c getseed.f
 
-g77 -g "$FILE_BASE"_c.o "$FILE_BASE"_f.o getseed.o -o "$FILE_BASE"_out
+#g77 -g "$FILE_BASE"_c.o "$FILE_BASE"_f.o getseed.o -o "$FILE_BASE"_out
+g77 -g libg2c.so.0 "$FILE_BASE"_c.o "$FILE_BASE"_f.o getseed.o -o "$FILE_BASE"_out
 
 #cp "$FILE_BASE"_out "$WEB_ROOT/$FILE_BASE.cgi"
 cp "$FILE_BASE"_out "$WEB_ROOT/simplerand3.cgi"
